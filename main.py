feb3af7592d8b47d84f96332b9f807215c4ad76d
ajithSubrahmanya <ajithmanjukody@gmail.com>
2021-10-21 10:06:36 +0530

    Standalone video made downloadable

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from pytube import YouTube
-video_list = [https://www.youtube.com/watch?v=ddaObxkRQH0]
+video_list = [ "https://www.youtube.com/watch?v=ddaObxkRQH0" ]
 
 for i in video_list:
     try:
         yt = YouTube(i)
-        print("Downloading the link :" +i)
-        print("Downloading the video :" +yt.streams[0].title)
+        print( "Downloading the link :" +i )
+        print( "Downloading the video :" +yt.streams[0].title )
     except:
         print("Connection error")
     stream = yt.streams.filter(res="144p").first()
