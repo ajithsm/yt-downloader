f48619c72fc870a85dfdaad5ed4563b409e0e94c
ajithSubrahmanya <ajithmanjukody@gmail.com>
2021-10-21 11:23:58 +0530

    Upgraded to 720p downloads

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pytube import YouTube
-video_list = [ "https://www.youtube.com/watch?v=ddaObxkRQH0" ]
+video_list = [ "https://www.youtube.com/watch?v=FC2dbAZcewo" ]
 
 for i in video_list:
     try:
@@ -8,6 +8,8 @@ for i in video_list:
         print( "Downloading the video :" +yt.streams[0].title )
     except:
         print("Connection error")
-    stream = yt.streams.filter(res="144p").first()
-    stream.download("Downloads/")
+
+    stream = yt.streams.filter(fps="60fps",res="720p").first()
+    stream.download(output_path="Downloads/")
 print("Task Completed")
+
